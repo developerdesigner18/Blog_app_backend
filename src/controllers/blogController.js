c672e72a17c654bab5d686e7319ce684738446e1
developerdesigner18 <developer.designer18@gmail.com>
2024-01-08 11:42:57 +0530

    Add: CRUD APIs for the Blogs activity

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -1,5 +1,49 @@
 const Blog = require("../models/Blog");
 
+const getAllPost = async (req, res) => {
+  try {
+    const blogs = await Blog.find();
+    if (blogs) {
+      return res.status(200).json({
+        code: 200,
+        success: true,
+        message: "Blog list",
+        data: blogs,
+      });
+    } else {
+      return res.status(300).json({
+        code: 300,
+        success: false,
+        message: "Something went wrong while fetching blog",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const getPost = async (req, res) => {
+  try {
+    const blog = await Blog.find({ _id: req.params.blogId });
+    if (blog) {
+      return res.status(200).json({
+        code: 200,
+        success: true,
+        message: "Blog find successfully",
+        data: blog,
+      });
+    } else {
+      return res.status(300).json({
+        code: 300,
+        success: false,
+        message: "Something went wrong while fetching blog",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 const createPost = async (req, res) => {
   const { title, content } = req.body;
   const author = req.user.id;
@@ -11,7 +55,7 @@ const createPost = async (req, res) => {
       author,
     });
     const savedBlog = await newBlog.save();
-    res.status(200).json({
+    return res.status(200).json({
       code: 200,
       success: true,
       message: "Post created successfully",
@@ -22,4 +66,53 @@ const createPost = async (req, res) => {
   }
 };
 
-module.exports = { createPost };
+const updatePost = async (req, res) => {
+  try {
+    const { blogId, ...dataToUpdate } = req.body;
+    const updateBlog = await Blog.updateOne(
+      { _id: blogId },
+      { $set: dataToUpdate }
+    );
+    if (updateBlog) {
+      return res.status(200).json({
+        code: 200,
+        success: true,
+        message: "Blog Updated successfully",
+        data: updateBlog,
+      });
+    } else {
+      return res.status(300).json({
+        code: 300,
+        success: false,
+        message: "Something went wrong while updating blog",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const deletePost = async (req, res) => {
+  try {
+    const blogId = req.params.blogId;
+    const deleteBlog = await Blog.deleteOne({ _id: blogId });
+    if (deleteBlog) {
+      return res.status(200).json({
+        code: 200,
+        success: true,
+        message: "Blog Deleted successfully",
+        data: deleteBlog,
+      });
+    } else {
+      return res.status(300).json({
+        code: 300,
+        success: false,
+        message: "Something went wrong while deleting blog",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { getAllPost, getPost, createPost, updatePost, deletePost };
--- src/routes/blogRoutes.js
+++ src/routes/blogRoutes.js
@@ -1,9 +1,19 @@
 const express = require("express");
-const { createPost } = require("../controllers/blogController");
+const {
+  createPost,
+  updatePost,
+  deletePost,
+  getAllPost,
+  getPost,
+} = require("../controllers/blogController");
 const { verifyToken } = require("../middleware/authenticator");
 
 const router = express.Router();
 
+router.get("/getAllPost", getAllPost);
+router.get("/getPost/:blogId", getPost);
 router.post("/createPost", verifyToken, createPost);
+router.post("/updatePost", verifyToken, updatePost);
+router.delete("/deletePost/:blogId", verifyToken, deletePost);
 
 module.exports = router;
